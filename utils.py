73448f536d689e00fbc204adb411380e63731b4f
ligerlac <lino.oscar.gerlach@cern.ch>
2024-08-07 13:34:21 +0200

    remove unused imports

--- utils.py
+++ utils.py
@@ -1,6 +1,4 @@
 import numpy as np
-import json
-# from sklearn.model_selection import train_test_split
 
 
 def save_data(images, labels, path):
@@ -20,22 +18,3 @@ def load_data(path) -> tuple:
     with open(f'{path}/labels.npy', 'rb') as f:
         labels = np.load(f)
     return images, labels
-
-
-# def save_data(images, labels, path):
-#     with open(f'{path}/images.npy', 'wb') as f:
-#         np.save(f, images)
-#     with open(f'{path}/labels.json', 'w') as f:
-#         json.dump(labels, f)
-
-
-# def load_data(path) -> tuple:
-#     """
-#     load data from path and return it in the following format:
-#     (train_images, train_labels), (test_images, test_labels)
-#     """
-#     with open(f'{path}/images.npy', 'rb') as f:
-#         images = np.load(f)
-#     with open(f'{path}/labels.json', 'r') as f:
-#         labels = json.load(f)
-#     return images, labels
